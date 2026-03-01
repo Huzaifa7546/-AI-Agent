85579344ec8a9a76287f58bff36f503b614e6ac2
huzaifa iqbal <huzaifaiqbal2018@gmail.com>
2026-03-01 06:38:30 +0530

    Fix syntax error in main.py

--- main.py
+++ main.py
@@ -6,4 +6,6 @@ from langchain_anthropic import chatanthropic
 load_dotenv()
 llm=chatopenai(model="gpt-4o-mini")
 llm2=chatanthropic(model="claude-3-5-sonnent-20240924"
+                   "abcd"
+
                    )
